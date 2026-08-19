eeaaf180a54ef818265110143cf4398e0696ebab
Vittorio Guerriero <vittorio.gue@gmail.com>
2026-08-19 21:57:28 +0100

    add input controller

--- src/main.rs
+++ src/main.rs
@@ -56,14 +56,17 @@ fn main() -> Result<(), Box<dyn Error>> {
         draw_display_to_console(&emulator);
 
         if poll(Duration::from_millis(10))? {
+            let evt = read()?;
             if let Event::Key(KeyEvent {
                 code: KeyCode::Char('c'),
                 modifiers: KeyModifiers::CONTROL,
                 ..
-            }) = read()?
+            }) = evt
             {
                 break;
             }
+
+            emulator.input = read_emulator_input(evt);
         }
     }
 
@@ -71,6 +74,36 @@ fn main() -> Result<(), Box<dyn Error>> {
     crossterm::terminal::disable_raw_mode().expect("to disable raw mode");
 
     Ok(())
+}
+
+fn read_emulator_input(evt: Event) -> u16 {
+    if let Event::Key(KeyEvent { code, .. }) = evt {
+        let key_value = match code {
+            KeyCode::Down => 8,
+            KeyCode::Up => 2,
+            KeyCode::Right => 6,
+            KeyCode::Left => 4,
+            KeyCode::Char('z') => 1,
+            KeyCode::Char('x') => 3,
+            KeyCode::Char('c') => 0xC,
+            KeyCode::Char('v') => 5,
+            KeyCode::Char('b') => 0xD,
+            KeyCode::Char('a') => 7,
+            KeyCode::Char('s') => 9,
+            KeyCode::Char('d') => 0xE,
+            KeyCode::Char('f') => 0xA,
+            KeyCode::Char('g') => 0,
+            KeyCode::Char('h') => 0xF,
+            KeyCode::Char('n') => 0xB,
+            _ => return 0,
+        };
+
+        let base = 0b0000000000000001;
+
+        return base << key_value;
+    }
+
+    return 0;
 }
 
 fn load_rom(emulator: &mut Chip8Emulator, rom: Vec<u8>) {
